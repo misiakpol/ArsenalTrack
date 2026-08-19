787b89718f3c6b3b83008a4de44269069c76d3a3
Adrian <misiakpol@gmail.com>
2026-08-19 13:08:49 +0200

    Update main tag height to h-max for better layout control

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
   return (
     <div className="light flex bg-gray-50 text-gray-900 w-full min-h-screen">
       <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
-      <main className="flex-1 flex-col min-w-0 h-full bg-gray-50">
+      <main className="flex-1 flex-col min-w-0 h-max bg-gray-50">
         <Navbar
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
